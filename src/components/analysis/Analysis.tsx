7dd71dfcb87ba0ec0bc4c44f8a0bb72d30e0faff
Jacek Krajewski <jacekkrajewski12@wp.pl>
2021-09-27 14:15:42 +0200

    fix component name error

--- src/components/analysis/Analysis.tsx
+++ src/components/analysis/Analysis.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Nav from '../pageElements/nav/Nav';
 import { useActions } from '../../redux/hooks/useActions';
 import { useTypedSelector } from '../../redux/hooks/useTypedSelector';
-import Modules from './modules/Modules';
+import Modules from './modules/modules';
 
 const Analysys: React.FC = () => {
   const { getFullDiary } = useActions();
